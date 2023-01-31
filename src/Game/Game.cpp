3624ed25c8a0fde83baf596c696d6cf8c4fe93a2
hhwrxhh <ivan.bohdan88y@gmail.com>
2023-01-31 15:30:45 +0200

    rewriting level

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -90,31 +90,13 @@ bool Game::init()
         return false;
     }
 
-    auto pTextureAtlas = ResourceManager::getTexture("mapTextureAtlas");
-    if (!pTextureAtlas)
-    {
-        std::cerr << "Error::Game::init ->Can not find texture atlas" << "mapTextureAtlas" << std::endl;
-        return false;
-    }
-
-    auto pTanksTextureAtlas = ResourceManager::getTexture("tanksTextureAtlas");
-    if (!pTanksTextureAtlas)
-    {
-        std::cerr << "Error::Game::init ->Can not find tank atlas" << "tanksTextureAtlas" << std::endl;
-        return false;
-    }
-
     glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(m_WinSize.x), 0.f, static_cast<float>(m_WinSize.y), -100.f, 100.f);
 
     pSpriteShaderProgram->use();
     pSpriteShaderProgram->setInt("tex", 0);
     pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
 
-    m_pTank = std::make_unique<Tank>(ResourceManager::getSprite("tankSprite_top"),
-                                     ResourceManager::getSprite("tankSprite_bottom"),
-                                     ResourceManager::getSprite("tankSprite_left"),
-                                     ResourceManager::getSprite("tankSprite_right"),
-                                     0.0000001f, glm::vec2(0), glm::vec2(16.f, 16.f));
+    m_pTank = std::make_unique<Tank>(0.0000001f, glm::vec2(0), glm::vec2(16.f, 16.f));
 
     m_pLevel = std::make_unique<Level>(ResourceManager::getLevels()[0]);
     return true;
--- src/Game/GameObjects/BrickWall.h
+++ src/Game/GameObjects/BrickWall.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <array>
 
 #include "IGameObject.h"
 
@@ -11,10 +12,56 @@ namespace RenderEngine
 class BrickWall : public IGameObject
 {
 public:
-	BrickWall(const std::shared_ptr<RenderEngine::Sprite> pSprite, const glm::vec2& position, const glm::vec2& size, const float rotation);
+
+	enum class EBrickWallType
+	{
+		All,
+		Top,
+		Bottom,
+		Left,
+		Right,
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight
+	};
+
+	enum class EBrickState {
+		All = 0,
+		TopLeft,
+		TopRight,
+		Top,
+		BottomLeft,
+		Left,
+		TopRight_BottomLeft,
+		Top_BottomLeft,
+		BottomRight,
+		TopLeft_BottomRight,
+		Right,
+		Top_BottomRight,
+		TopLeft_Bottom,
+		TopRight_Bottom,
+		Bottom,
+		Destroyed
+	};
+
+	enum class EBrickLocation
+	{
+		TopLeft,
+		TopRight,
+		BottomLeft, 
+		BottomRight
+	};
+
+	BrickWall(const EBrickWallType eBrickWallType, const glm::vec2& position, const glm::vec2& size, const float rotation);
 	virtual void render() const override;
 	virtual void update(uint64_t delta) override;
 
 private:
-	std::shared_ptr<RenderEngine::Sprite> m_pCurrentSprite;
+	void renderBrick(const EBrickLocation eBrickLocation) const;
+
+	std::array<std::shared_ptr<RenderEngine::Sprite>, 15> m_sprites;
+	std::array<EBrickState, 4> m_eCurrentBrickState;
+
+
 };
